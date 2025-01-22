973fdb8fd111497e75f9ad6b9a96a775400c9ee5
Daniel Madera <dmadera0@gmail.com>
2025-01-22 13:58:35 -0800

    print all contacts list

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 try:
   from contact import contacts
   print(contacts)
-  
-  
 except ModuleNotFoundError:
   print("Error: contacts.py file not found.")
 
+def names_list(contacts):
+    return ["Name"] in contacts()
+
+print(names_list)
