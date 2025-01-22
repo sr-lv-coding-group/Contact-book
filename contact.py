3aa132cb12e007086296941a9f25b337d19cedc6
Daniel Madera <dmadera0@gmail.com>
2025-01-22 12:51:16 -0800

    added contacts

--- contact.py
+++ contact.py
@@ -1,3 +1,64 @@
-Daniel = P("Daniel")
-Keegan = p("keegan")
-Dennis =P("dennis")
+contacts = {
+    1: {
+        "Name": "Daniel",
+        "Age": 35,
+        "PhoneNumber": 5555551212,
+        "Location": "Las Vegas"
+    },
+    
+    2: {
+        "Name": "Denis",
+        "Age": 30,
+        "PhoneNumber": 1234567890,
+        "Location": "New York"
+    },
+    3: {
+        "Name": "Keegan",
+        "Age": 30,
+        "PhoneNumber": 4569876543,
+        "Location": "New York"
+    },
+    4: {
+        "Name": "Neil",
+        "Age": 30,
+        "PhoneNumber": 5551112222,
+        "Location": "New York"
+    },
+    5: {
+        "Name": "Matt",
+        "Age": 30,
+        "PhoneNumber": 1233334444,
+        "Location": "New York"
+    },
+    6: {
+        "Name": "Alice",
+        "Age": 30,
+        "PhoneNumber": 1234567890,
+        "Location": "New York"
+    },
+    7: {
+        "Name": "Heather",
+        "Age": 30,
+        "PhoneNumber": 5557778888,
+        "Location": "New York"
+    },
+    8: {
+        "Name": "John",
+        "Age": 30,
+        "PhoneNumber": 1239990000,
+        "Location": "New York"
+    },
+    9: {
+        "Name": "Amanda",
+        "Age": 30,
+        "PhoneNumber": 4562223333,
+        "Location": "New York"
+    },
+    10: {
+        "Name": "Zach",
+        "Age": 30,
+        "PhoneNumber": 5554445555,
+        "Location": "New York"
+    }
+
+}
