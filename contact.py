98aa41f15d1ef474c16f18c51a073f0d95564d5e
Daniel Madera <dmadera0@gmail.com>
2025-01-24 13:42:07 -0800

    age filter

--- contact.py
+++ contact.py
@@ -8,55 +8,55 @@ contacts = {
     
     2: {
         "Name": "Denis",
-        "Age": 30,
+        "Age": 27,
         "PhoneNumber": 1234567890,
         "Location": "New York"
     },
     3: {
         "Name": "Keegan",
-        "Age": 30,
+        "Age": 39,
         "PhoneNumber": 4569876543,
         "Location": "New York"
     },
     4: {
         "Name": "Neil",
-        "Age": 30,
+        "Age": 58,
         "PhoneNumber": 5551112222,
         "Location": "New York"
     },
     5: {
         "Name": "Matt",
-        "Age": 30,
+        "Age": 32,
         "PhoneNumber": 1233334444,
         "Location": "New York"
     },
     6: {
         "Name": "Alice",
-        "Age": 30,
+        "Age": 25,
         "PhoneNumber": 1234567890,
         "Location": "New York"
     },
     7: {
         "Name": "Heather",
-        "Age": 30,
+        "Age": 50,
         "PhoneNumber": 5557778888,
         "Location": "New York"
     },
     8: {
         "Name": "John",
-        "Age": 30,
+        "Age": 21,
         "PhoneNumber": 1239990000,
         "Location": "New York"
     },
     9: {
         "Name": "Amanda",
-        "Age": 30,
+        "Age": 36,
         "PhoneNumber": 4562223333,
         "Location": "New York"
     },
     10: {
         "Name": "Zach",
-        "Age": 30,
+        "Age": 22,
         "PhoneNumber": 5554445555,
         "Location": "New York"
     }
