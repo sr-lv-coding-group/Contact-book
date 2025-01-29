1ba22a2243065837bdd57c24291555cda8642481
Daniel Madera <dmadera0@gmail.com>
2025-01-29 12:07:23 -0800

    removed contacts from index

--- index.py
+++ index.py
@@ -4,47 +4,27 @@ try:
 except ModuleNotFoundError:
   print("Error: contacts.py file not found.")
 
-# def names_list(contacts):
-#     return ["Name"] in contacts()
-
-# print(names_list(contacts))
-
-# def get_phone_number(contacts, name):
-#   for contact_id, contact_info in contacts.items():
-#     if contact_info["Name"].lower() == name.lower():
-#       return contact_info["PhoneNumber"]
-#     return None
-  
-# name_to_search = input("Enter name: ")
-
-# phone_number = get_phone_number(contacts, name_to_search)
-
-# if phone_number:
-#   print(f"{name_to_search.capitalize()} phone number is {phone_number}")
-# else:
-#   print(f"No contact found for {name_to_search}")
-
-contacts = {
-    1: {
-        "Name": "Daniel",
-        "Age": 35,
-        "PhoneNumber": 5555551212,
-        "Location": "Las Vegas"
-    },
+# contacts = {
+#     1: {
+#         "Name": "Daniel",
+#         "Age": 35,
+#         "PhoneNumber": 5555551212,
+#         "Location": "Las Vegas"
+#     },
     
-    2: {
-        "Name": "Dennis",
-        "Age": 27,
-        "PhoneNumber": 1234567890,
-        "Location": "New York"
-    },
-    3: {
-        "Name": "Keegan",
-        "Age": 39,
-        "PhoneNumber": 4569876543,
-        "Location": "New York"
-    },
-}
+#     2: {
+#         "Name": "Dennis",
+#         "Age": 27,
+#         "PhoneNumber": 1234567890,
+#         "Location": "New York"
+#     },
+#     3: {
+#         "Name": "Keegan",
+#         "Age": 39,
+#         "PhoneNumber": 4569876543,
+#         "Location": "New York"
+#     },
+# }
 
 def find_contact(contacts, name):
   for contact_id, contact_info in contacts.items():
