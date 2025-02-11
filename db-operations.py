fc81016a4a33ce9c17dec151b4ed990c0f96b343
keegan <keeganheilman@gmail.com>
2025-02-11 14:40:45 -0800

    write connection and execution operations

--- db-operations.py
+++ db-operations.py
@@ -2,47 +2,72 @@ import sqlite3
 
 # collection of functions to perform basic db operations (C.R.U.D.)
 # note: to check validity, sqlite3.complete_statement("SELECT foo FROM bar;")
-# CREATE / INSERT
-
-
-# READ / SELECT
-
-
-# UPDATE / UPDATE
-
-
-# DELETE /DELETE
-
 
 # connect to DB (create DB if non-existent)
-def connect_db(db_name):
+def db_connect(db_name):
+    directory = "."
     try:
+        file_path = os.path.join(directory, db_name)
+        # Check if the file exists at the specified path.
+        assert(os.path.exists(os.path.join(directory, db_name)))
         conn = sqlite3.connect(db_name)
         print(f"Success! Able to connect to db: {db_name}")
+        return conn.cursor()
     except (NameError):
         print(f"Failure! Unable to create/connect to db: {db_name}"/
               + "\n\n" + NameError)
+    return None
+
+
+def db_execute_cmd(sql_cmd, cur):
+    try:
+        return cur.execute(sql_cmd)
+    except e:
+        print("Failure!" + "\n" + e)
+        return None
+
 
 ########################
-# HELPER functions
+# HELPER FUNCTIONS
 ########################
-# request name of object
-def request_object_name(obj):
-    return input(f"Enter the name of the {obj}:")
+def request_sql_cmd(sql_cmd):
+    return input(f"Please type SQL statement to execute")
+
+
+########################
+# MENU OPTIONS
+########################
+# define menu options
 
 
 if __name__ == "__main__":
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Testing... db-operations.py \n")
-    print("Please select an option:")
-    print("1. Connect Database (w/implicit creation)")
-    print("5. Exit")
+    db_name = input(f"Write the name of the database you wish to connect: ")
+    cur = db_connect(db_name)
     
-    choice = input("Enter your choice (1-5): ")
-    match choice:
-        case "1":
-            db_name = request_object_name(obj="database")            
-            connect_db(db_name)
-        case "5":
-            print("Exiting... ")
-        case _:
-            print("Invalid selection. Please choose from the available options.\n")
+    choice = "1"
+    while (choice != "2"):
+        print("\n\n")
+        print("Please select an option:")
+        print("1. Execute SQL Statement")
+        print("2. Exit")
+        choice = input("Enter your choice (1 or 2): ")
+        match choice:
+            case "1":
+                print("selection is '1' - Execute SQL statement")
+                sql_cmd = input("Type SQL command:")
+                if (sqlite3.complete_statement(sql_cmd)):
+                    print(f"Valid SQL command: {sql_cmd}")
+                    result = db_execute_cmd(sql_cmd, cur)
+                    print(result.fetchall())
+                else:
+                    print(f"Invalid SQL command! {sql_cmd}")
+                    break
+            case "2":
+                print("selection is '2' - Exit")
+            case _:
+                print("Invalid selection. Please choose from the available options.\n")
+
+print("Exiting... ")
